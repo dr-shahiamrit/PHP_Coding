683ee2da15bd74014f34788914ba8d5a09e06dbc
Amrit Shahi <dr@shahiamrit365.com.np>
2024-05-25 22:55:59 +0545

    added

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// $num = 1; 
+
+// $num1 = 30;
+
+// echo $num + $num1;
+
+
+// Defining Constant
+/*
+define('num1', 25); // read only value
+
+echo num1;
+*/
+
+//num1 = 13   // get error because you are trying to assign value to constant 
+
+
+/*
+// Case Sencitive
+define ('num1', 25, true);
+echo NUM1;
+
+*/
+
+// Function in php
